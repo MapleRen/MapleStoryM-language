33f303eaa5c854992a51eaaf261c067feabc4bb4
MapleRen <kl941104@163.com>
2020-03-16 13:04:26 +0800

    圈X 测试脚本

--- Kor/QuantumultX/Clear/VersionRedirect.js
+++ Kor/QuantumultX/Clear/VersionRedirect.js
@@ -1,8 +1,4 @@
-console.log('进入了Redirect.js')
-console.log(JSON.stringify($request))
-
 if($request.url.indexOf('_1/') > -1){
-    console.log('重定向'+$request.url.replace('_1/','/'))
     var mStatus = "HTTP/1.1 302 Found";
     var mHeaders = {"Location": $request.url.replace('_1/','/')};
     var mResponse = {
