aea3dd54e8539e8ed684e5bd171843507344de57
MapleRen <kl941104@163.com>
2020-03-16 23:12:51 +0800

    圈X 汉化脚本测试0.1.0

--- Kor/QuantumultX/Translate/BaseRewrite.js
+++ Kor/QuantumultX/Translate/BaseRewrite.js
@@ -7,32 +7,34 @@ const body = $response.body;
 let list = body.split('\n');
 const files = ['data.bin.lan.kor.tbl', 'data.table.unity3d']
 const prefix = 'msm_kor_needRedirect'
-$task.fetch(api_url).then(response => {
+$task.fetch(config).then(response => {
     if (response.statusCode != 200) {
         $notify("汉化失败", "", 'API请求过于频繁，请稍后再试');
         $prefs.setValueForKey('false', prefix)
         $done({});
     }
     let data = JSON.parse(response.body);
-    let tbl_size = data.find(x => x.name = 'data.bin.lan.kor.tbl').size;
-    if (body.indexOf(tbl_size) === -1) {
-        $notify("汉化失败", "", '汉化文件未更新，请关注微博"冒险岛M第三汉化委"获取最新消息');
-        $prefs.setValueForKey('false', prefix)
-        $done({});
-    } else {
+    let tbl_size = data.find(x => x.name == 'data.bin.lan.kor.tbl').size;
+    if (body.indexOf(tbl_size) > -1) {
         for (let i = 0; i < list.length; i++) {
-            const flag = files.filter(item => list[i].indexOf(x)).length > 0;
-            if (flag) {
-                let item = data.find(x => x.name = flag[0]);
-                list[i] = list[i].replace(/Size="[0-9.]*?"/i, `Size="${item.size}"`).replace(/FileCRC="[0-9.]*?"/i, `FileCRC=""`).replace(/CRC="[0-9.]*?"/i, `CRC=""`).replace(/OriginalCRC="[0-9.]*?"/i, `OriginalCRC=""`)
+            const file = files.filter(item => list[i].indexOf(item) > -1);
+            if (file.length > 0) {
+                let item = data.find(x => x.name == file[0]);
+                list[i] = list[i].replace(/Size="[0-9.]*?"/i, 'Size="'+item.size+'"').replace(/FileCRC="[0-9.]*?"/i, 'FileCRC=""').replace(/ CRC="[0-9.]*?"/i, ' CRC=""').replace(/OriginalCRC="[0-9.]*?"/i, 'OriginalCRC=""')
             }
         }
         $notify("汉化完成", "", '补丁下载完成即可完成汉化');
         $prefs.setValueForKey('true', prefix)
-        $done(list.join('\n'));
+        var xml = list.join('\n');
+        $done(xml);
+        
+    } else {
+        $notify("汉化失败", "", '汉化文件未更新，请关注微博"冒险岛M第三汉化委"获取最新消息');
+        $prefs.setValueForKey('false', prefix)
+        $done({});
     }
 }, reason => {
     $notify("基础汉化失败", "", reason.error);
     $prefs.setValueForKey('false', prefix)
-    $done({});
+    $done(body);
 });
--- Kor/QuantumultX/Translate/ProRewrite.js
+++ Kor/QuantumultX/Translate/ProRewrite.js
@@ -1,38 +1,42 @@
 //用于获取仓库里data.bin.lan.kor.tbl的size，与最新xml里的Size做比较
 const config ={
-    API:'https://api.github.com/repos/MapleRen/MapleStoryM-language/contents/?ref=master',
+    API:'http://api.github.com/repos/MapleRen/MapleStoryM-language/contents/?ref=master',
     files:['data.bin.lan.kor.tbl', 'data.table.unity3d','data.language_kor.unity3d'],
     prefix:'msm_kor_needRedirect',
     title:'进阶汉化'
 }
+
 const body = $response.body;
 let list = body.split('\n');
-$task.fetch(config.API).then(response => {
+const prefix = 'msm_kor_needRedirect'
+$task.fetch({url:config.API}).then(response => {
     if (response.statusCode != 200) {
-        $notify(`${title}失败`, "", 'API请求过于频繁，请稍后再试');
-        $prefs.setValueForKey('false', config.prefix)
+        $notify(config.title, "", 'API请求过于频繁，请稍后再试');
+        $prefs.setValueForKey('false', prefix)
         $done({});
     }
     let data = JSON.parse(response.body);
-    let tbl_size = data.find(x => x.name = 'data.bin.lan.kor.tbl').size;
-    if (body.indexOf(tbl_size) === -1) {
-        $notify(`${title}失败`, "", '汉化文件未更新，请关注微博"冒险岛M第三汉化委"获取最新消息');
-        $prefs.setValueForKey('false', config.prefix)
-        $done({});
-    } else {
+    let tbl_size = data.find(x => x.name == 'data.bin.lan.kor.tbl').size;
+    if (body.indexOf(tbl_size) > -1) {
         for (let i = 0; i < list.length; i++) {
-            const flag = config.files.filter(item => list[i].indexOf(x)).length > 0;
-            if (flag) {
-                let item = data.find(x => x.name = flag[0]);
-                list[i] = list[i].replace(/Size="[0-9.]*?"/i, `Size="${item.size}"`).replace(/FileCRC="[0-9.]*?"/i, `FileCRC=""`).replace(/CRC="[0-9.]*?"/i, `CRC=""`).replace(/OriginalCRC="[0-9.]*?"/i, `OriginalCRC=""`)
+            const file = config.files.filter(item => list[i].indexOf(item) > -1);
+            if (file.length > 0) {
+                let item = data.find(x => x.name == file[0]);
+                list[i] = list[i].replace(/Size="[0-9.]*?"/i, 'Size="'+item.size+'"').replace(/FileCRC="[0-9.]*?"/i, 'FileCRC="0"').replace(/ CRC="[0-9.]*?"/i, ' CRC="0"').replace(/OriginalCRC="[0-9.]*?"/i, 'OriginalCRC="0"')
             }
         }
-        $notify(`${title}完成`, "", '补丁下载完成即可完成汉化');
-        $prefs.setValueForKey('true', config.prefix)
-        $done(list.join('\n'));
+        $notify(config.title, "", '补丁下载完成即可完成汉化');
+        $prefs.setValueForKey('true', prefix)
+        var xml = list.join('\n');
+        $done(xml);
+        
+    } else {
+        $notify(config.title, "", '汉化文件未更新，请关注微博"冒险岛M第三汉化委"获取最新消息');
+        $prefs.setValueForKey('false', prefix)
+        $done({});
     }
 }, reason => {
-    $notify(`${title}失败`, "", reason.error);
-    $prefs.setValueForKey('false', config.prefix)
-    $done({});
+    $notify(config.title, "", 'API请求失败，请重试');
+    $prefs.setValueForKey('false', prefix)
+    $done(body);
 });
