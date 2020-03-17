fa4f7688521a42c00f23c392920add33245fd268
MapleRen <kl941104@163.com>
2020-03-17 19:15:54 +0800

    修改

--- Kor_Test/QuantumultX/Script/Clear.js
+++ Kor_Test/QuantumultX/Script/Clear.js
@@ -1,6 +1,6 @@
 var isMarketVersions = $request.url.indexOf('MarketVersion') > -1;
 var isAssetBundleTable = $request.url.indexOf('AssetBundle_table.xml') > -1;
-
+var isRedirect = $request.url.indexOf('_1/') > -1;
 if (isMarketVersions) {
     var body = $response.body;
     var list = body.split('\n');
@@ -11,7 +11,15 @@ if (isMarketVersions) {
     }
     var xmlData = list.join('\n')
     $done(xmlData)
-} else if(isAssetBundleTable){
+} else if (isRedirect) {
+    var mStatus = "HTTP/1.1 302 Found";
+    var mHeaders = { "Location": $request.url.replace('_1/', '/') };
+    var mResponse = {
+        status: mStatus,
+        headers: mHeaders
+    }
+    $done(mResponse);
+} else if (isAssetBundleTable) {
     var fileList = ['data.bin.lan.kor.tbl', 'data.language_kor.unity3d', 'data.table.unity3d']
     var body = $response.body;
     var list = body.split('\n');
@@ -24,15 +32,7 @@ if (isMarketVersions) {
     $notify("冒险岛M", "", "文件重置完成，弹出更新框后(也可下载完毕后)关闭游戏，切换模式后重新进入游戏即可完成汉化");
     var xmlData = list.join('\n')
     $done(xmlData)
-}else if($request.url.indexOf('_1/') > -1){
-    var mStatus = "HTTP/1.1 302 Found";
-    var mHeaders = {"Location": $request.url.replace('_1/','/')};
-    var mResponse = {
-        status:mStatus,
-        headers:mHeaders
-    }
-    $done(mResponse);
-}else{
+} else {
     $done({});
 }
 
