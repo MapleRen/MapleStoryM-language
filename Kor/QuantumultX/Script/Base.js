228d6da06858ae73d7f5f00b2209c3a5999537b2
MapleRen <kl941104@163.com>
2020-03-22 18:14:11 +0800

    圈X 汉化脚本 0.6.2

--- Kor/QuantumultX/Script/Base.js
+++ Kor/QuantumultX/Script/Base.js
@@ -7,27 +7,26 @@ const config ={
 }
 
 function rewrite() {
-    const body = $response.body;
-    let list = body.split('\n');
-    const regionReg = /.*Region=\"(.*?)\".*/gm;
-    const crcReg = /.*<Header CRC=\"(.*?)\".*/gm;
-    const region = body.replace(regionReg,'$1');
-    const xmlCRC = body.replace(crcReg,'$1');   
-    $task.fetch({url:config.API}).then(response => {
+    let body = compressXml($response.body);
+    const crcReg = /.*<Header CRC=\"(.*?)\"([^\[]*)/gm;
+      
+    const xmlCRC = body.replace(crcReg,'$1');$task.fetch({url:config.API}).then(response => {
+
         if (response.statusCode != 200) {
             notifyAndSetValue('XML请求失败，请重试','false');
             $done({});
         }
-        const latestXml = response.body;
+        const latestXml = compressXml(response.body);
         const latestXmlCRC = latestXml.replace(crcReg,'$1');
         if(xmlCRC != latestXmlCRC){
             notifyAndSetValue('汉化文件未更新，请关注微博"冒险岛M第三汉化委"获取最新消息','false');
             $done({});
         }else{
-            for (let i = 0; i < files.length; i++) {
-                const fileCRC = latestXml.getXmlAttr(files[i],"FileCRC");
-                const fileSize = latestXml.getXmlAttr(files[i],"Size");
-                body = body.setXmlAttr(body,files[i],"FileCRC",fileCRC).setXmlAttr(body,files[i],"CRC",fileCRC).setXmlAttr(body,files[i],"Size",fileSize);
+            for (let i = 0; i < config.files.length; i++) {
+                const file = config.files[i]
+                const fileCRC = latestXml.getXmlAttr(file,"FileCRC");
+                const fileSize = latestXml.getXmlAttr(file,"Size");
+                body = body.setXmlAttr(file,"FileCRC",fileCRC).setXmlAttr(file,"CRC",fileCRC).setXmlAttr(file,"Size",fileSize);
             }
             notifyAndSetValue('补丁下载完成即可完成汉化','true');
             //console.log(body);
@@ -40,14 +39,25 @@ function rewrite() {
 }
 
 String.prototype.getXmlAttr =function(path,attr){
-    var reg = new RegExp(`^(.*Path=\"${path}\"[^\>]*${attr}}=\")(\\d+)(\".*)`)
+    const reg = new RegExp(`^(.*Path=\"${path}\"[^\>]*${attr}=\")(\\d+)(\".*)`)
     return this.replace(reg,'$2')
 }
 String.prototype.setXmlAttr =function(path,attr,value){
-    var reg = new RegExp(`^(.*Path=\"${path}\"[^\>]*${attr}}=\")\\d+(\".*)`)
+    const reg = new RegExp(`^(.*Path=\"${path}\"[^\>]*${attr}=\")\\d+(\".*)`)
     return this.replace(reg,`$1${value}$2`)
 }
 
+function compressXml(xml){
+    const rep = /\n+/g;
+    const repone = /<!--.*?-->/ig;
+    const reptwo = /\/\*.*?\*\//ig;
+    const reptree = /[ ]+</ig;
+    let sourceZero = xml.replace(rep,"").replace(/[\r\n]/g,"");
+    let sourceOne = sourceZero.replace(repone,"");
+    let sourceTwo = sourceOne.replace(reptwo,"");
+    let sourceTree = sourceTwo.replace(reptree,"<");
+    return sourceTree;
+}
 
 function notifyAndSetValue(msg,success){
     $notify(config.title, "", msg);
@@ -59,9 +69,9 @@ function redirect() {
     const need_redirect = $prefs.valueForKey(config.prefix);
     const file_name = $request.url.slice($request.url.lastIndexOf('@') + 1);
     if (need_redirect == 'true') {
-        var mStatus = "HTTP/1.1 302 Found";
-        var mHeaders = { "Location": `${github_path}${file_name}` };
-        var mResponse = {
+        const mStatus = "HTTP/1.1 302 Found";
+        const mHeaders = { "Location": `${github_path}${file_name}` };
+        const mResponse = {
             status: mStatus,
             headers: mHeaders
         }
